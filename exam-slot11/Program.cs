1dfa10b903a0803093ee4ea2adc6de9c2231b715
Vanh020104 <114908216+Vanh020104@users.noreply.github.com>
2023-08-17 15:25:20 +0700

    Add project files.

---
+++ exam-slot11/Program.cs
@@ -0,0 +1,29 @@
+﻿using exam_slot11.demo1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam_slot11
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BackAccount account = new BackAccount();
+
+            account.SoDuThayDoi += Account_SoDuThayDoi;
+
+
+            account.Deposit(1000000);
+            account.Withdraw(1000000);
+        }
+
+        private static void Account_SoDuThayDoi(object sender, EventArgs e)
+        {
+            BackAccount account = (BackAccount)sender;
+            Console.WriteLine($"Bien dong so du: {account.Balance}");
+        }
+    }
+}
---
+++ exam-slot11/demo1/BackAccount.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam_slot11.demo1
+{
+    public class BackAccount
+    {
+
+        private decimal balance;
+        public decimal Balance
+        {
+            get { return balance; } 
+            private set
+            {
+                if (value != balance)
+                {
+                    balance = value;
+                    OnBalanceChanged();
+                }
+            }
+        }
+        public event EventHandler SoDuThayDoi;
+
+        protected virtual void OnBalanceChanged()
+        {
+            SoDuThayDoi?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Deposit (decimal amount)
+        {
+            if (amount <= 0) 
+            {
+                throw new ArgumentException("Amount must be positive.", nameof(amount));
+                
+            }
+            Balance += amount;
+        }
+
+
+
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Amount must be positive.", nameof(amount));
+            }
+
+            if (amount > Balance)
+            {
+                throw new InvalidOperationException("Insufficient balance.");
+            }
+
+            Balance -= amount;
+        }
+    }
+
+}
+
